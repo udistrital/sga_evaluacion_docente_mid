b9ce36df1cb72a6bcd9c41f97d8f1b75eff2386a
LethSphere <lethsphere@gmail.com>
2024-08-18 20:54:36 -0500

    feat: udistrital/sga_documentacion#419

--- services/formulario_services.go
+++ services/formulario_services.go
@@ -3,86 +3,148 @@ package services
 import (
 	"fmt"
 
-	"github.com/beego/beego"
+	"github.com/astaxie/beego"
 	"github.com/udistrital/sga_evaluacion_docente_mid/helpers"
 	"github.com/udistrital/utils_oas/request"
 	"github.com/udistrital/utils_oas/requestresponse"
 )
 
-type FormularioService struct {
-	// Aquí puedes definir los campos y métodos necesarios para tu servicio
-}
-
 func ConsultaFormulario(id_tipo_formulario string, id_periodo string, id_tercero string, id_espacio string) (APIResponseDTO requestresponse.APIResponse) {
 	fmt.Println("Entró")
 
-	var secciones []map[string]interface{}
-
-	var plantilla []map[string]interface{}
-	errPlantilla := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("plantilla?query=EstructuraId:%v", id_tipo_formulario), &plantilla)
+	var plantilla map[string]interface{}
+	errPlantilla := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("plantilla?query=ProcesoId:%v&Activo:true&sortby=Id&order=asc&limit=0", id_tipo_formulario), &plantilla)
 	if errPlantilla != nil || fmt.Sprintf("%v", plantilla) == "[map[]]" {
+		fmt.Println(plantilla)
 		return helpers.ErrEmiter(errPlantilla, fmt.Sprintf("%v", plantilla))
 	}
 
-	for _, p := range plantilla {
-
-		var seccion map[string]interface{}
-		errSeccion := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("seccion/%v", p["SeccionId"]), &seccion)
-		if errSeccion != nil || fmt.Sprintf("%v", seccion) == "[map[]]" {
-			return helpers.ErrEmiter(errSeccion, fmt.Sprintf("%v", seccion))
-		}
-
-		var items []map[string]interface{}
-		errItems := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("item?query=Id:%v", p["ItemId"]), &items)
-		if errItems != nil || fmt.Sprintf("%v", items) == "[map[]]" {
-			return helpers.ErrEmiter(errItems, fmt.Sprintf("%v", items))
-		}
-
-		var itemsStruct map[string]interface{}
-		for _, item := range items {
-
-			var itemCampos []map[string]interface{}
-			errItemCampos := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("item_campo?query=ItemId:%v", item["Id"]), &itemCampos)
-			if errItemCampos != nil || fmt.Sprintf("%v", itemCampos) == "[map[]]" {
-				return helpers.ErrEmiter(errItemCampos, fmt.Sprintf("%v", itemCampos))
-			}
-
-			var camposStruct []map[string]interface{}
-			for _, campo := range itemCampos {
-
-				var campoDetalle map[string]interface{}
-				errCampoDetalle := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("campo/%v", campo["CampoId"]), &campoDetalle)
-				if errCampoDetalle != nil || fmt.Sprintf("%v", campoDetalle) == "[map[]]" {
-					return helpers.ErrEmiter(errCampoDetalle, fmt.Sprintf("%v", campoDetalle))
-				}
-
-				camposStruct = append(camposStruct, map[string]interface{}{
-					"tipoCampo": campoDetalle["TipoCampoId"],
-					"nombre":    campoDetalle["Nombre"],
-					"valor":     campoDetalle["Valor"],
-				})
-			}
-
-			itemsStruct[item["Nombre"].(string)] = map[string]interface{}{
-				"orden":    item["Orden"],
-				"campos":   camposStruct,
-				"tipoItem": item["TipoItem"],
-			}
-		}
-
-		secciones = append(secciones, map[string]interface{}{
-			"orden":         seccion["Orden"],
-			"nombreSeccion": seccion["Nombre"],
-			"items":         itemsStruct,
-		})
+	var itemCampos map[string]interface{}
+	errItemCampos := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("item_campo?query=Activo:true&sortby=Id&order=asc&limit=0"), &itemCampos)
+	if errItemCampos != nil || fmt.Sprintf("%v", itemCampos) == "[map[]]" {
+		fmt.Println(itemCampos)
+		return helpers.ErrEmiter(errItemCampos, fmt.Sprintf("%v", itemCampos))
 	}
 
+	var campos map[string]interface{}
+	errCampos := request.GetJson("http://"+beego.AppConfig.String("EvaluacionDocenteService")+fmt.Sprintf("campo?query=Activo:true&sortby=Id&order=asc&limit=0"), &campos)
+	if errCampos != nil || fmt.Sprintf("%v", campos) == "[map[]]" {
+		fmt.Println(campos)
+		return helpers.ErrEmiter(errCampos, fmt.Sprintf("%v", campos))
+	}
+
+	secciones := map[int]map[string]interface{}{}
+	data := plantilla["Data"].([]interface{})
+	itemCamposMap := make(map[int][]map[string]interface{})
+	itemCamposData := itemCampos["Data"].([]interface{})
+	camposData := campos["Data"].([]interface{})
+	for _, itemCampo := range itemCamposData {
+		itemCampoMap := itemCampo.(map[string]interface{})
+		itemId := int(itemCampoMap["ItemId"].(map[string]interface{})["Id"].(float64))
+		campo := itemCampoMap["CampoId"].(map[string]interface{})
+		campoId := int(campo["Id"].(float64))
+
+		campoInfo := map[string]interface{}{
+			"nombre":     campo["Nombre"].(string),
+			"tipo_campo": int(campo["TipoCampoId"].(float64)),
+			"valor":      campo["Valor"],
+			"porcentaje": itemCampoMap["Porcentaje"],
+			"escala":     obtenerCamposHijos(campoId, camposData), //funcion para encontrar los campos hijos si los hay si no regresa lo mismo
+		}
+		itemCamposMap[itemId] = append(itemCamposMap[itemId], campoInfo)
+	}
+
+	for _, item := range data {
+		itemMap := item.(map[string]interface{})
+		seccion := itemMap["SeccionId"].(map[string]interface{})
+		seccionId := int(seccion["Id"].(float64))
+
+		if _, exists := secciones[seccionId]; !exists {
+			secciones[seccionId] = map[string]interface{}{
+				"nombre": seccion["Nombre"].(string),
+				"orden":  int(seccion["Orden"].(float64)),
+				"items":  []map[string]interface{}{},
+			}
+		}
+
+		itemId := int(itemMap["ItemId"].(map[string]interface{})["Id"].(float64))
+		itemInfo := map[string]interface{}{
+			"id":     itemId,
+			"nombre": itemMap["ItemId"].(map[string]interface{})["Nombre"].(string),
+			"orden":  int(itemMap["ItemId"].(map[string]interface{})["Orden"].(float64)),
+			"campos": itemCamposMap[itemId],
+		}
+		secciones[seccionId]["items"] = append(secciones[seccionId]["items"].([]map[string]interface{}), itemInfo)
+	}
+
+	ordenSecciones := map[string]interface{}{}
+	ordenNombres := map[int]string{
+		0: "descripcion",
+		1: "cuantitativa",
+		2: "cualitativa",
+		3: "carga de archivos",
+		4: "descarga de archivos",
+	}
+
+	for _, seccion := range secciones {
+		seccionMap := seccion
+		orden := seccionMap["orden"].(int)
+		nombre := ordenNombres[orden]
+		if nombre == "" {
+			nombre = fmt.Sprintf("seccion_%d", orden)
+		}
+		ordenSecciones[nombre] = seccionMap
+	}
+
+	//Aqui queadaría organizada por secciones
 	response := map[string]interface{}{
-		"tipoEvaluacion":   "Evaluación de ejemplo",
-		"docente":          "Nombre del docente",
-		"espacioAcademico": "Espacio académico",
-		"seccion":          secciones,
+		"Success": true,
+		"Status":  200,
+		"Message": "Consulta exitosa",
+		"Data": map[string]interface{}{
+			"docente":          id_tercero, //consultar cuando exista la data del tercero evaluado
+			"espacioAcademico": id_espacio, //consultar cuando exista la data del espacio academico
+			"seccion":          ordenSecciones,
+			"tipoEvaluacion":   id_tipo_formulario, //consultar a parametro  y se le pasa el id del tipo de evaluacion
+		},
 	}
 
 	return requestresponse.APIResponseDTO(true, 200, response, "Consulta exitosa")
+}
+
+func obtenerCamposHijos(campoId int, camposData []interface{}) []map[string]interface{} {
+	var hijos []map[string]interface{}
+
+	var campoPadre map[string]interface{}
+	for _, campo := range camposData {
+		campoMap := campo.(map[string]interface{})
+		if int(campoMap["Id"].(float64)) == campoId {
+			campoPadre = campoMap
+			break
+		}
+	}
+
+	if campoPadre != nil && int(campoPadre["CampoPadreId"].(float64)) == 0 {
+		hijoInfo := map[string]interface{}{
+			"nombre":     campoPadre["Nombre"].(string),
+			"tipo_campo": int(campoPadre["TipoCampoId"].(float64)),
+			"valor":      campoPadre["Valor"],
+		}
+		hijos = append(hijos, hijoInfo)
+		return hijos
+	}
+
+	for _, campo := range camposData {
+		campoMap := campo.(map[string]interface{})
+		if int(campoMap["CampoPadreId"].(float64)) == campoId {
+			hijoInfo := map[string]interface{}{
+				"nombre":     campoMap["Nombre"].(string),
+				"tipo_campo": int(campoMap["TipoCampoId"].(float64)),
+				"valor":      campoMap["Valor"],
+			}
+			hijos = append(hijos, hijoInfo)
+		}
+	}
+
+	return hijos
 }
