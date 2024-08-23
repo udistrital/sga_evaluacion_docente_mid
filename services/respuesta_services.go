59b6d82ac05e58d32704eaf647f1a40a33e01d98
LethSphere <lethsphere@gmail.com>
2024-08-23 07:39:58 -0500

    feat: udistrital/sga_documentacion#427

--- services/respuesta_services.go
+++ services/respuesta_services.go
@@ -1,5 +1,56 @@
 package services
 
-func GuardarRespuestas() {
+import (
+	"encoding/json"
+	"fmt"
 
+	"github.com/udistrital/utils_oas/requestresponse"
+)
+
+func GuardarRespuestas(data []byte) (APIResponseDTO requestresponse.APIResponse) {
+
+	var dataSource map[string]interface{}
+	var respuestas []map[string]interface{}
+	var nuevaRespuesta map[string]interface{}
+
+	if err := json.Unmarshal(data, &dataSource); err != nil {
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, "Error al parsear el JSON")
+		return APIResponseDTO
+	}
+
+	if r, ok := dataSource["respuestas"].([]interface{}); ok {
+		for _, resp := range r {
+			if item, ok := resp.(map[string]interface{}); ok {
+				metadata := make(map[string]interface{})
+
+				if itemID, ok := item["item_id"]; ok {
+					metadata["item_id"] = itemID
+				}
+				if valor, ok := item["valor"]; ok {
+					metadata["valor"] = valor
+				}
+				if archivos, ok := item["archivos"]; ok {
+					metadata["archivos"] = archivos
+				}
+
+				nuevaRespuesta = map[string]interface{}{
+					"Activo":            true,
+					"FechaCreacion":     "2024-08-23T00:00:00Z",
+					"FechaModificacion": "2024-08-23T00:00:00Z",
+					"Metadata":          metadata,
+				}
+
+				//err := guardar
+
+				fmt.Println(nuevaRespuesta)
+				respuestas = append(respuestas, nuevaRespuesta)
+			}
+		}
+	} else {
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, "No se encontraron respuestas válidas")
+		return APIResponseDTO
+	}
+
+	APIResponseDTO = requestresponse.APIResponseDTO(true, 200, respuestas, nil)
+	return APIResponseDTO
 }
