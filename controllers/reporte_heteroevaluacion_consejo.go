34db64bacead072cee815c594821a78ed1a52298
JulianAvella <julianavella01@gmail.com>
2025-05-27 16:02:26 -0500

    feat: Se desarrolla la logica para recibir correctamente los parametros del envio de correo con pdf, se refactoriza para mejorar el codigo en mejores practicas udistrital/sga_documentacion#573

--- controllers/reporte_heteroevaluacion_consejo.go
+++ controllers/reporte_heteroevaluacion_consejo.go
@@ -3,11 +3,6 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/udistrital/sga_evaluacion_docente_mid/services"
-	"github.com/udistrital/sga_evaluacion_docente_mid/models"
-
-	"github.com/udistrital/sga_evaluacion_docente_mid/helpers"
-	"log"
-	"fmt"
 )
 
 // ReporteHeteroevaluacionConsejoController operations for ReporteHeteroevaluacionConsejo
@@ -30,66 +25,6 @@ func (c *ReporteHeteroevaluacionConsejoController) URLMapping() {
 // @Failure 400 parámetros inválidos
 // @Failure 500 error al ejecutar la consulta
 func (c *ReporteHeteroevaluacionConsejoController) Get() {
-
-	b64, err := helpers.CrearCertificadoAutoevaluacion(
-		"OMER CALDERON",
-		"12119277",
-		"601 - DOCTORADO INTERINSTITUCIONAL EN EDUCACIÓN",
-		"18 del mes 05 de 2025",
-	)
-	if err != nil {
-		log.Fatalf("Error creando PDF: %v", err)
-	}
-	fmt.Println("Base64 del PDF:", b64)
-
-	
-	//////////
-
-	////////
-
-
-	data := models.EmailTemplateData{
-		NombreUsuario:     "BARON CAMACHO LUZ AMPARO",
-		DocumentoUsuario:  "12.345.678",
-		NombreEvaluacion:  "Heteroevaluación",
-		NumeroPeriodo:     "61",
-		FechaEvaRealizada: "07 de mayo de 2025",
-		HoraEvaRealizada:  "09:40 AM",
-	}
-
-	adjuntos := []models.EmailAttachment{
-		{
-			ContentType: "application/pdf",
-			FileName:    "certificado.pdf",
-			Base64File:  b64, 
-		},
-	}
-
-	respuesta := services.EnviarEmail(
-		[]string{"jerodrigueza@udistrital.edu.co"},
-		"condor@udistrital.edu.co",
-		"PLANTILLA_EVALUACION_DOCENTE",
-		data,
-		adjuntos,
-	)
-
-	fmt.Println("Resultado del envío:", respuesta)
-
-	// sirve la creación del pdf
-
-	/*b64, err := helpers.CrearCertificadoAutoevaluacion(
-		"OMER CALDERON",
-		"12119277",
-		"601 - DOCTORADO INTERINSTITUCIONAL EN EDUCACIÓN",
-		"18 del mes 05 de 2025",
-	)
-	if err != nil {
-		log.Fatalf("Error creando PDF: %v", err)
-	}
-	fmt.Println("Base64 del PDF:", b64)*/
-
-	
-	//////////
 	evaluadoId := c.GetString("evaluado_id")
 	periodoId, err1 := c.GetInt("periodo_id")
 	procesoId, err2 := c.GetInt("proceso_id")
--- helpers/utils.go
+++ helpers/utils.go
@@ -5,13 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-
 	"github.com/pdfcpu/pdfcpu/pkg/api"
-
 	"bytes"
 	"github.com/phpdave11/gofpdf"
-	//"math"
-
 )
 
 func MergeBase64PDFs(base64Docs []string) (string, error) {
@@ -47,14 +43,25 @@ func MergeBase64PDFs(base64Docs []string) (string, error) {
 	return mergedB64, nil
 }
 
-func CrearCertificadoAutoevaluacion(nombre string, cedula string, proyecto string, fecha string) (string, error) {
-	//espacio académico = materia
+func CrearCertificadoPdfEvaluacion(
+	nombre, cedula, espacioAcademico, fecha, nombreEva, periodoAcademico, nombreEvaluado, grupo string,
+) (string, error) {
+	const (
+		margenIzquierdo = 25.0
+		margenSuperior  = 20.0
+		margenDerecho   = 25.0
+		anchoPagina     = 216.0
+		altoPagina      = 280.0
+		col1Width       = 96.0
+		col2Width       = 70.0
+	)
+
 	pdf := gofpdf.NewCustom(&gofpdf.InitType{
 		OrientationStr: "P",
 		UnitStr:        "mm",
-		Size:           gofpdf.SizeType{Wd: 216, Ht: 280}, 
+		Size:           gofpdf.SizeType{Wd: anchoPagina, Ht: altoPagina},
 	})
-	pdf.SetMargins(25, 20, 25)
+	pdf.SetMargins(margenIzquierdo, margenSuperior, margenDerecho)
 	pdf.AddPage()
 
 	pdf.AddUTF8Font("arial", "", "utils/fonts/arial.ttf")
@@ -68,58 +75,76 @@ func CrearCertificadoAutoevaluacion(nombre string, cedula string, proyecto strin
 	pdf.CellFormat(0, 10, "CERTIFICA QUE:", "", 1, "C", false, 0, "")
 	pdf.Ln(14)
 
-	pdf.SetFont("arial", "", 10)
-	pdf.Write(6, "El docente ")
-	pdf.SetFont("arial", "B", 10)
-	pdf.Write(6, nombre)
-	pdf.SetFont("arial", "", 10)
-	pdf.Write(6, ", identificado con cédula de ciudadanía No. ")
-	pdf.SetFont("arial", "B", 10)
-	pdf.Write(6, cedula)
-	pdf.SetFont("arial", "", 10)
-	pdf.Write(6, ", inscrito en el proyecto curricular listado a continuación, ha realizado la autoevaluación docente para el periodo académico 2025-1 así:")
-	pdf.Ln(14)
-	
-	col1Width := 96.0 
-	col2Width := 70.0 
-	pdf.SetFont("arial", "B", 10)
-	pdf.CellFormat(col1Width, 6, "PROYECTO CURRICULAR", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(col2Width, 6, "EVALUACIÓN DOCENTE", "1", 1, "C", false, 0, "")
-	pdf.SetFont("arial", "", 10)
-	x := pdf.GetX()
-	y := pdf.GetY()
-	pdf.MultiCell(col1Width, 5.5, proyecto, "1", "C", false)
-	h1 := pdf.GetY() - y 
-	pdf.SetXY(x+col1Width, y)
-	pdf.CellFormat(col2Width, h1, fecha, "1", 0, "C", false, 0, "")
-	pdf.SetY(y + h1)
-	pdf.Ln(14)
+	agregarContenido := func(textoInicial, evaluado string) {
+		pdf.SetFont("arial", "", 10)
+		pdf.Write(6, textoInicial)
+		pdf.SetFont("arial", "B", 10)
+		pdf.Write(6, evaluado)
+		pdf.SetFont("arial", "", 10)
+	}
+
+	agregarTabla := func(col1, col2 string) {
+		pdf.SetFont("arial", "B", 10)
+		pdf.CellFormat(col1Width, 6, col1, "1", 0, "C", false, 0, "")
+		pdf.CellFormat(col2Width, 6, "TIPO EVALUACIÓN", "1", 1, "C", false, 0, "")
+		pdf.SetFont("arial", "", 10)
+
+		x := pdf.GetX()
+		y := pdf.GetY()
+
+		pdf.MultiCell(col1Width, 5.5, col2, "1", "C", false)
+		h := pdf.GetY() - y
+		pdf.SetXY(x+col1Width, y)
+		pdf.CellFormat(col2Width, h, nombreEva, "1", 0, "C", false, 0, "")
+		pdf.SetY(y + h)
+		pdf.Ln(14)
+	}
+
+	switch nombreEva {
+		case "Autoevaluación I", "Heteroevaluación":
+			agregarContenido("El (La) estudiante ", nombre)
+			agregarContenido(", identificado (a) con No. ", cedula)
+			pdf.Write(6, fmt.Sprintf(", ha realizado la evaluación docente para el periodo académico %s así:", periodoAcademico))
+			pdf.Ln(14)
+			agregarTabla("ESPACIO ACADÉMICO", espacioAcademico)
+
+		case "Coevaluación I":
+			agregarContenido("La persona docente ", nombre)
+			agregarContenido(", identificado (a) con cédula de ciudadanía No. ", cedula)
+			pdf.Write(6, fmt.Sprintf(", ha realizado la evaluación docente para el grupo %s en el periodo académico %s así:", grupo, periodoAcademico))
+			pdf.Ln(14)
+			agregarTabla("ESPACIO ACADÉMICO", espacioAcademico)
+
+		case "Autoevaluación II 3", "Autoevaluación II 2", "Autoevaluación II 1":
+			agregarContenido("La persona docente ", nombre)
+			agregarContenido(", identificado (a) con cédula de ciudadanía No. ", cedula)
+			pdf.Write(6, fmt.Sprintf(", ha realizado la evaluación docente para el periodo académico %s así:", periodoAcademico))
+			pdf.Ln(14)
+			agregarTabla("ESPACIO ACADÉMICO", espacioAcademico)
+
+		case "Coevaluación II":
+			agregarContenido("La persona ", nombre)
+			agregarContenido(", identificado (a) con No. ", cedula)
+			pdf.Write(6, fmt.Sprintf(", ha realizado la evaluación docente para el periodo académico %s así:", periodoAcademico))
+			pdf.Ln(14)
+			agregarTabla("DOCENTE EVALUADO (A)", nombreEvaluado)
+
+		default:
+			return "", fmt.Errorf("tipo de evaluación no reconocido: %s", nombreEva)
+	}
 
 	pdf.SetFont("arial", "", 10)
-	textoFinalNormal := fmt.Sprintf("La presente certificación se expide automáticamente desde el Sistema de Gestión Académica el día %s.",
-		fecha)
-	pdf.MultiCell(0, 6, textoFinalNormal, "", "L", false)
+	pdf.MultiCell(0, 6, fmt.Sprintf("La presente certificación se expide automáticamente desde el Sistema de Gestión Académica el día %s.", fecha), "", "L", false)
 	pdf.Ln(7)
-
-	pdf.SetFont("arial", "", 10)
 	pdf.MultiCell(0, 6, "Atento saludo,", "", "L", false)
 	pdf.Ln(7)
-	
 	pdf.SetFont("arial", "B", 10)
 	pdf.MultiCell(0, 6, "COORDINACIÓN OFICINA DE EVALUACIÓN DOCENTE\nUNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS\nSISTEMA SOPORTADO POR LA OATI", "", "L", false)
 
-	// Guardar en buffer
 	var buf bytes.Buffer
 	if err := pdf.Output(&buf); err != nil {
 		return "", fmt.Errorf("error generando PDF: %w", err)
 	}
 
-	// Guardar PDF para revisión visual
-	if err := os.WriteFile("Certificado-Autoevaluacion.pdf", buf.Bytes(), 0o644); err != nil {
-		return "", fmt.Errorf("error escribiendo archivo: %w", err)
-	}
-
-	// Codificar base64
 	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
-
 }
